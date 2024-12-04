817fad9677b6f59b5c48aa6335ca7a7c21b841dc
Rujun Yan <1141653364@qq.com>
2024-12-04 00:17:16 -0500

    a6

--- index.js
+++ index.js
@@ -9,11 +9,14 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js"
 import EnrollmentRoutes from './Kanbas/Enrollments/routes.js';
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    origin: "http://localhost:3000" || process.env.NETLIFY_URL,
 })); 
 const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kanbas",
