ae1a4570405201fb59c35b07eabfd6d47cf6f1b5
Rujun Yan <1141653364@qq.com>
2024-12-05 14:34:16 -0500

    a6

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -1,14 +1,17 @@
-import Database from "../Database/index.js";
-export function enrollUserInCourse(userId, courseId) {
-  const { enrollments } = Database;
-  enrollments.push({ _id: Date.now(), user: userId, course: courseId });
+import model from "./model.js";
+
+export async function findCoursesForUser(userId) {
+ const enrollments = await model.find({ user: userId }).populate("course");
+ return enrollments.map((enrollment) => enrollment.course);
+}
+export async function findUsersForCourse(courseId) {
+ const enrollments = await model.find({ course: courseId }).populate("user");
+ return enrollments.map((enrollment) => enrollment.user);
+}
+export function enrollUserInCourse(user, course) {
+ return model.create({ user, course });
+}
+export function unenrollUserFromCourse(user, course) {
+ return model.deleteOne({ user, course });
 }
 
-export function deleteEnrollment(userId, courseId) {
-  const { enrollments } = Database;
-  const updatedEnrollments = enrollments.filter(
-    (enrollment) => !(enrollment.user === userId && enrollment.course === courseId)
-  );
-  Database.enrollments = updatedEnrollments;
-  return true;
-}
