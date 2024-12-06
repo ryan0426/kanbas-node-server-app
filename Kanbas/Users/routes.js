0452338fd2f88e721bd29cfdbdd9e15e5167e638
Rujun Yan <1141653364@qq.com>
2024-12-06 12:55:38 -0500

    a6

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -88,11 +88,11 @@ export default function UserRoutes(app) {
 
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
+  app.post("/api/users/signin", signin);
   app.get("/api/users/:userId", findUserById);
   app.put("/api/users/:userId", updateUser);
   app.delete("/api/users/:userId", deleteUser);
   app.post("/api/users/signup", signup);
-  app.post("/api/users/signin", signin);
   app.post("/api/users/signout", signout);
   app.post("/api/users/profile", profile);
 
